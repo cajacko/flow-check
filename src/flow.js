02bc5d4858a4047d6b74979559888436abff9701
Charlie Jackson <charlie@charliejackson.com>
2017-03-10 16:55:42 +0000

    Find correct flow-cli path

--- src/flow.js
+++ src/flow.js
@@ -1,9 +1,16 @@
 const spawn = require('child_process').spawnSync;
 const path = require('path');
 const getFlowArgs = require('./args').getFlowArgs;
+var fs = require('fs');
 
 module.exports = function flow() {
-  let args = [path.join(__dirname, '../node_modules/flow-bin/cli.js')];
+  let cliPath = path.join(__dirname, '../node_modules/flow-bin/cli.js');
+
+  if (!fs.existsSync(path)) {
+    cliPath = path.join(__dirname, '../../flow-bin/cli.js');
+  }
+
+  let args = [cliPath];
   args = args.concat(getFlowArgs());
 
   return spawn('node', args, { cwd: process.cwd(), stdio: 'inherit' });
